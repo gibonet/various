e008a1fa96fc065ecbe07a6d0b4130b9294731e3
gibonet <gibo.gaf@gmail.com>
2016-09-01 23:09:19 +0200

    Add files via upload

--- shp_sp_example.R
+++ shp_sp_example.R
@@ -16,3 +16,42 @@ library(sp)
 str(s)
 
 plot(s)
+
+# The coordinates of the SpatialPolygonsDataFrame can be extracted and
+# saved in a "tidy" data frame with ggplot2::fortify or broom::tidy
+d <- broom::tidy(s)
+str(d)
+
+# In order to properly define regions, look at the data in the 
+# SpatialPolygonsDataFrame (s)
+str(s@data)
+
+# Packages rgeos and maptools are needed
+library(broom)
+library(rgeos)
+library(maptools)
+d <- tidy(s, region = "id")
+
+str(d)
+head(d)
+
+# Now the coordinates are in a data frame, that can be plotted for example
+# with ggplot2 or ggvis. See some examples in shp_df_example.R
+
+# One further step: adding all columns of the SpatialPolygonsDataFrame
+# in the tidy data frame
+library(dplyr)
+d <- d %>% left_join(s@data, by = "id")
+
+str(d)
+
+
+# Note that shp_sp was tranformed in a list of data frames with the following
+# commands (the first line can take quite a long time):
+shp_df <- lapply(shp_sp, function(x) ggplot2::fortify(x, region = "id"))
+for(i in seq_along(shp_df)){
+  shp_df[[i]] <- left_join(shp_df[[i]], shp_sp[[i]]@data, by = "id")
+}
+
+# Since this takes quite a long time (about 10 minutes on my computer)
+# I saved the list shp_df in file shp_df.rda in data folder.
