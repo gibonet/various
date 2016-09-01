3255f8e96233b486009ab9ce70a89d00a50d59d4
gibonet <gibo.gaf@gmail.com>
2016-09-01 21:53:57 +0200

    Add files via upload

---
+++ shp_df_example.R
@@ -0,0 +1,26 @@
+
+load("data/shp_df.rda")
+
+class(shp_df)
+length(shp_df)
+
+unique(sapply(shp_df, class))
+
+# Every element of the list is a data frame
+names(shp_df)
+
+# Give a look of one of these data frames
+d <- shp_df[["g1k15"]]
+
+str(d)
+head(d)
+
+# These are the coordinates (lon and lat) of swiss cantons (in 2015)
+
+# These can be plotted for example with ggplot2
+library(ggplot2)
+
+p <- ggplot(data = d, aes(x = lon, y = lat, group = group)
+
+p + geom_path() + coord_equal()
+
